3b74dc4d350e62e8e90e6003d0fc41f9fb9211b7
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-23 17:47:09 +0300

    переделка сервера

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose');
-const app = require('./app.js');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app.js");
 
-const DB_HOST = 'mongodb+srv://vadim:K7MUuaXcmiGL8zhM@cluster0.zn74clk.mongodb.net/db-contacts?retryWrites=true&w=majority';
-mongoose.set('strictQuery', true);
+const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.connect(DB_HOST)
-  .then(() => {
-  app.listen(3000)
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-})
+mongoose.set("strictQuery", true);
+
+try {
+  mongoose.connect(DB_HOST);
+
+  app.listen(PORT, () => {
+    console.log("Database connection successful");
+  });
+} catch (error) {
+  console.log(error.message);
+  process.exit(1);
+} finally {
+  mongoose.disconnect();
+}
+
