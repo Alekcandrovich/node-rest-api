57c566d6de1080b88c38f65441f91f623599c32a
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-23 19:20:37 +0300

    убрал DB_HOST

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app.js');
 
-const DB_HOST = 'mongodb+srv://vadim:K7MUuaXcmiGL8zhM@cluster0.zn74clk.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
