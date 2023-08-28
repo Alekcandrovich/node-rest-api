e02b769dc926e453556467ea2cbd453cff2e11a1
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-29 01:04:23 +0300

    сделал helpers middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,10 @@
+
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
+const auth = require("./auth");
 
 module.exports = {
   isValidId,
   validateBody,
+  auth,
 };
