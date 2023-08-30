69a3128dfe8c696ce876906f8e7058ac3a63308b
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-30 17:02:11 +0300

    пагинация для контактов

--- controllers/contacts/get.js
+++ controllers/contacts/get.js
@@ -2,7 +2,7 @@ const { Contact } = require('../../models/contact');
 
 const list = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10, favorite = null } = req.query;
+  const { page = 1, limit = 20, favorite = null } = req.query;
   const skip = (page - 1) * limit;
   const query = { owner };
 
