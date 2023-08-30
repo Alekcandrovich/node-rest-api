196fd0aeee6acffc56d92cbeb8bc44c694f5e9c2
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-30 14:26:32 +0300

    код в папке helpers готов

--- helpers/ctrlWrapper.js
+++ helpers/ctrlWrapper.js
@@ -1,12 +1,12 @@
 const ctrlWrapper = (ctrl) => {
-  const func = async (req, res, next) => {
+  const wrapper = async (req, res, next) => {
     try {
       await ctrl(req, res, next);
     } catch (err) {
       next(err);
     }
   };
-  return func;
+  return wrapper;
 };
 
 module.exports = ctrlWrapper;
--- helpers/handleError.js
+++ helpers/handleError.js
@@ -1,6 +1,8 @@
 const handleError = (error, _, next) => {
   const { name, code } = error;
-  const status = name === 'MongoServerError' && code === 11000 ? 409 : 400;
+  const status = name === 'MongoServerError' && code === 11000 ?
+    (409, 'Эта электронная почта уже используется') :
+    (400, 'Неверный запрос');
   error.status = status;
   next();
 };
