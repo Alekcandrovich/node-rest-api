c8d1e6d043b664a0de4cf9a4b730f603ff1b81b1
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-31 23:30:22 +0300

    решается проблема с пагинацией

--- controllers/contacts/get.js
+++ controllers/contacts/get.js
@@ -1,12 +1,24 @@
-const { Contact } = require('../../models/contact');
+// const { Contact } = require('../../models/contact');
 
-const list = async (req, res) => {
+// const list = async (req, res) => {
+//   const { _id: owner } = req.user;
+//   const { page = 1, limit = 20 } = req.query;
+//   const skip = (page - 1) * limit;
+//   const result = await Contact.find(owner, { skip, limit, }).populate('owner', 'name email');
+//   console.log(result);
+//   res.json(result);
+// };
+
+// module.exports = list;
+
+
+const { Contact } = require("../../models/contact");
+
+const add = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
-  const skip = (page - 1) * limit;
-  const result = await Contact.find(owner, { skip, limit, }).populate('owner', 'name email');
-  console.log(result);
-  res.json(result);
+  console.log(owner);
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
 };
 
-module.exports = list;
+module.exports = add;
