afc6524f4910e428574f9059b9323594e8b64752
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-09-01 15:26:32 +0300

    все ошибки исправлены - код полностью работает

--- controllers/contacts/get.js
+++ controllers/contacts/get.js
@@ -1,24 +1,16 @@
-// const { Contact } = require('../../models/contact');
+const { Contact } = require('../../models/contact');
 
-// const list = async (req, res) => {
-//   const { _id: owner } = req.user;
-//   const { page = 1, limit = 20 } = req.query;
-//   const skip = (page - 1) * limit;
-//   const result = await Contact.find(owner, { skip, limit, }).populate('owner', 'name email');
-//   console.log(result);
-//   res.json(result);
-// };
-
-// module.exports = list;
-
-
-const { Contact } = require("../../models/contact");
-
-const add = async (req, res) => {
+const list = async (req, res) => {
   const { _id: owner } = req.user;
-  console.log(owner);
-  const result = await Contact.create({ ...req.body, owner });
-  res.status(201).json(result);
+  console.log(req.user);
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner })
+    .skip(skip)
+    .limit(limit)
+    .populate("owner", "email");
+  console.log(result);
+  res.json(result);
 };
 
-module.exports = add;
+module.exports = list;
